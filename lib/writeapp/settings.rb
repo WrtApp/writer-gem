6c0a2d26a02fce3628c7e1b9a26226799abcb7ac
Bill Patrianakos <bill@chooseclever.com>
2013-03-08 15:23:47 -0600

    Working on the settings and setup functionality

--- lib/writeapp/settings.rb
+++ lib/writeapp/settings.rb
@@ -5,12 +5,22 @@ require 'uri'
 
 module Writeapp
 	class Settings
-		def exists
+		def account_exists
 			# Set up account checking method. Check for settings file in ~/
 		end
 
-		def setup
-			# Code to setup existing account
+		def connect_account (username, password)
+			file = File.expand_path('.writeapp', '~')
+			File.open(file, 'w') do |f|
+				f.puts "username: #{username}\npassword:#{password}"
+			end
+			puts "Username and password saved. If you need to change these in the future just run `writeapp --setup`"
+		end
+
+		def load_account
+			account_file = File.expand_path('.writeapp', '~')
+			account = YAML.load_file(account_file)
+			return account
 		end
 	end
 end
